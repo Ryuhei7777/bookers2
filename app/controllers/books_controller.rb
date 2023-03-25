97c9421f42cbb646783c91affbdd7cf6ebfaf256
Ryuhei7777 <124151763+Ryuhei7777@users.noreply.github.com>
2023-03-25 03:13:37 +0000

    [create]4-27投稿機能実装中

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,8 @@
 class BooksController < ApplicationController
   def index
     @user=current_user
+    @book=Book.new
+    @books= Book.all
   end
 
   def show
@@ -8,4 +10,22 @@ class BooksController < ApplicationController
 
   def edit
   end
+
+   # 投稿データの保存
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to book_path(@book.user_id)
+  end
+
+
+
+   # 投稿データのストロングパラメータ
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
 end
