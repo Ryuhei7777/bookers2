e102e280e04ecedba9cca8451ddc5551d65a2476
Ryuhei7777 <124151763+Ryuhei7777@users.noreply.github.com>
2023-03-28 17:32:18 +0000

    [create]4-27 ユーザ情報編集機能実装完了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,12 +7,26 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
+  def update
+    user = User.find(params[:id])
+    user.update(user_params)
+    redirect_to user_path(user)
+  end
+  
   def show
     @user = Book.find(params[:id]).user
     @book = Book.new
     @users = User.find(params[:id])
     @books = @users.books
   end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image, :introduction)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   get "/homes/about" => "homes#about", as: "about"
   resources :books, only: [:edit, :index, :show, :destroy, :create, :update]
-  resources :users, only: [:edit, :index, :show]
+  resources :users, only: [:edit, :index, :show, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
