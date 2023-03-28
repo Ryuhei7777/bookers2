2cdb0441a3894d31eb03e39ba61ec28212a49551
Ryuhei7777 <124151763+Ryuhei7777@users.noreply.github.com>
2023-03-28 16:19:30 +0000

    [create]4-27 投稿内容編集機能実装完了

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
   get "/homes/about" => "homes#about", as: "about"
-  resources :books, only: [:edit, :index, :show, :destroy, :create]
+  resources :books, only: [:edit, :index, :show, :destroy, :create, :update]
   resources :users, only: [:edit, :index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
