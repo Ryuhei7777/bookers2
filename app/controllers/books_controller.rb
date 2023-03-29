69e33d927477d4979b6e83242c052aa5360b0d1f
Ryuhei7777 <124151763+Ryuhei7777@users.noreply.github.com>
2023-03-29 05:33:35 +0000

    [create]4-27 アクセス制限実装完了

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,7 @@
 class BooksController < ApplicationController
   before_action :authenticate_user!
+  before_action :is_matching_login_user, only: [:edit, :update]
+
   def index
     @user = current_user
     @book = Book.new
@@ -13,13 +15,24 @@ class BooksController < ApplicationController
   end
 
   def edit
+    if Book.find(params[:id]) == current_user.books.id
     @book = Book.find(params[:id])
+    else
+      book =  current_user.books.id
+      redirect_to book_path(book)
+    end
   end
 
   def update
     book = Book.find(params[:id])
-    book.update(book_params)
+    if book.update(book_params)
+      flash[:notice] = "You have updated book successfully."
     redirect_to book_path(book.id)
+    else
+      @book = Book.find(params[:id])
+      @book.update(book_params)
+      render :edit
+    end
   end
 
   def destroy
@@ -32,8 +45,15 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
-    redirect_to book_path(@book)
+    if @book.save
+      flash[:notice] = "You have created book successfully."
+      redirect_to book_path(@book)
+    else
+      @user = current_user
+      @books = Book.all
+      render :index
+
+    end
   end
 
 
@@ -43,6 +63,14 @@ class BooksController < ApplicationController
 
   def book_params
     params.require(:book).permit(:title, :body)
+  end
+
+  def is_matching_login_user
+    user_id = params[:id].to_i
+    login_user_id = current_user.id
+    if(user_id != login_user_id)
+      redirect_to books_path
+    end
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-    before_action :authenticate_user!
+  before_action :authenticate_user!
   def index
     @user = current_user
     @book = Book.new
@@ -7,21 +7,31 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
+      @user = User.find(params[:id])
+      unless @user == current_user
+      redirect_to user_path(current_user)
+      end
   end
 
   def update
     user = User.find(params[:id])
-    user.update(user_params)
+    if user.update(user_params)
+      flash[:notice] = "You have updated user successfully."
     redirect_to user_path(user)
+    else
+      @user = User.find(params[:id])
+      @user.update(user_params)
+      render :edit
+    end
   end
-  
+
   def show
-    @user = Book.find(params[:id]).user
+    @user = User.find(params[:id])
     @book = Book.new
     @users = User.find(params[:id])
     @books = @users.books
   end
+
 
   private
 
